b805e23679096a18cf360e8329098ee522bf08d2
helenep <helene.pruvot1@gmail.com>
2015-03-02 22:38:43 +0100

    home playlist responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
       $("#header").load("header.html"); 
     });
 
+    $('#pl-nav-resp .item').on('click',function () {
+      $('#pl-nav-resp .item').not(this).removeClass('active');
+      $(this).addClass('active');
+      var index = $(this).index(),
+      newTarget = $('.playlist-content .row').eq(index).slideDown();
+      $('.playlist-content .row').not(newTarget).slideUp();
+    });
+
 });
