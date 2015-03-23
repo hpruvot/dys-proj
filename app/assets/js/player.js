4dbe3087df88ee226aeef817c8044f029f48fd54
helenep <helene.pruvot1@gmail.com>
2015-03-23 14:52:11 +0100

    player author

--- app/assets/js/player.js
+++ app/assets/js/player.js
@@ -5,6 +5,7 @@ var index = 0,
     extension = '.mp3',
     songTitle = $('#asaTitle'),
     songArtist = $('#asaArtist'),
+    songAuthor = $('#asaAuthor span'),
     songTotalTime = $('#atTotal'),
     songCurrentTime = $('#atCurrent'),
     songCover = $('#acCover'),
@@ -20,6 +21,7 @@ player.button = document.getElementById("audio-playpause");
 player.buttonMute = document.getElementById("audio-muteunmute");
 player.buttonPrev = document.getElementById("audio-prev");
 player.buttonNext = document.getElementById("audio-next");
+player.buttonShuffle = document.getElementById("audio-shuffle");
 player.pB = $('.audio-progress');
 
 player.audio.load();
@@ -63,50 +65,89 @@ player.setTime = function (e) {
     media.currentTime=e.offsetX*media.duration/$(this).width();
 }
 
-player.loadTrack = function(id) {
+player.loadTrack = function(id, newData) {
     var tracksTotal = 0;
-    $.ajax({
-        url : 'http://localhost:3000/assets/tracks.json',
-        dataType: "json",
-        beforeSend : function(){
-            $(player.audio)[0].pause();
-        },
-        success : function(data){
+    if(newData) {
+        console.log(newData);
+        id = 0;
+        var source = newData[id].file;
+        player.audio.setAttribute("src",""+mediaPath + source + extension+"");
+        // Track infos
+        songTitle.text(newData[id].name);
+        songArtist.text(newData[id].artist);
+        songAuthor.text(newData[id].author);
+        songTotalTime.text(newData[id].length);
 
-            var source = data[id].file;
-            player.audio.setAttribute("src",""+mediaPath + source + extension+"");
-            // Track infos
-            songTitle.text(data[id].name);
-            songArtist.text(data[id].artist);
-            songTotalTime.text(data[id].length);
-
-            if(data[id].cover) {
-                songCover.attr("src",coverPath + data[id].cover + ".jpg");
-            }else {
-                songCover.attr("src",coverPath + "default.png");
-            }
-            
-            // Play track
-            $(player.audio)[0].load();
-            $(player.audio)[0].play();
-
-            tracksTotal = data.length;
-
-            player.button.classList.remove('play');
-            player.button.classList.add('pause');
-
-            $("#plUL").empty();
-
-            for (var i = id+1; i < data.length; i++) {
-                if(data[i].cover) {
-                    data[i].cover = data[i].cover + ".jpg";
-                }else {
-                    data[i].cover = "default.png";
-                }
-                $("#plUL").append("<li><div class='plItem'><div class='plCover'><img src='"+coverPath+data[i].cover+"'></div><div class='plTitle'>"+data[i].name+"</div><div class='plArtist'>"+data[i].artist+"</div><div class='plAuthor'>par<span>"+data[i].author+"</span></div></div></li>");
-            }
+        if(newData[id].cover) {
+            songCover.attr("src",coverPath + newData[id].cover + ".jpg");
+        }else {
+            songCover.attr("src",coverPath + "default.png");
         }
-    });
+        
+        // Play track
+        $(player.audio)[0].load();
+        $(player.audio)[0].play();
+
+        tracksTotal = newData.length;
+
+        player.button.classList.remove('play');
+        player.button.classList.add('pause');
+
+        $("#plUL").empty();
+
+        for (var i = id+1; i < newData.length; i++) {
+            if(newData[i].cover) {
+                newData[i].cover = newData[i].cover + ".jpg";
+            }else {
+                newData[i].cover = "default.png";
+            }
+            $("#plUL").append("<li><div class='plItem'><div class='plCover'><img src='"+coverPath+newData[i].cover+"'></div><div class='plTitle'>"+newData[i].name+"</div><div class='plArtist'>"+newData[i].artist+"</div><div class='plAuthor'>par<span>"+newData[i].author+"</span></div></div></li>");
+        }
+    }
+    else {
+        $.ajax({
+            url : 'http://localhost:3000/assets/tracks.json',
+            dataType: "json",
+            beforeSend : function(){
+                $(player.audio)[0].pause();
+            },
+            success : function(data){
+                var source = data[id].file;
+                player.audio.setAttribute("src",""+mediaPath + source + extension+"");
+                // Track infos
+                songTitle.text(data[id].name);
+                songArtist.text(data[id].artist);
+                songAuthor.text(data[id].author);
+                songTotalTime.text(data[id].length);
+
+                if(data[id].cover) {
+                    songCover.attr("src",coverPath + data[id].cover + ".jpg");
+                }else {
+                    songCover.attr("src",coverPath + "default.png");
+                }
+                
+                // Play track
+                $(player.audio)[0].load();
+                $(player.audio)[0].play();
+
+                tracksTotal = data.length;
+
+                player.button.classList.remove('play');
+                player.button.classList.add('pause');
+
+                $("#plUL").empty();
+
+                for (var i = id+1; i < data.length; i++) {
+                    if(data[i].cover) {
+                        data[i].cover = data[i].cover + ".jpg";
+                    }else {
+                        data[i].cover = "default.png";
+                    }
+                    $("#plUL").append("<li><div class='plItem'><div class='plCover'><img src='"+coverPath+data[i].cover+"'></div><div class='plTitle'>"+data[i].name+"</div><div class='plArtist'>"+data[i].artist+"</div><div class='plAuthor'>par<span>"+data[i].author+"</span></div></div></li>");
+                }
+            }
+        });
+    }
 
     player.audio.onended = function(){
         player.loadTrack(id+1);
@@ -158,6 +199,25 @@ player.muteUnmute = function() {
     }
 }
 
+player.shuffle = function (e){
+    $.ajax({
+        url : 'http://localhost:3000/assets/tracks.json',
+        dataType: "json",
+        success : function(data){
+            var id = Math.floor(Math.random()*data.length);
+
+            if (id >= data.length) {
+                var k = id - data.length;
+                while ((k--) + 1) {
+                    data.push(undefined);
+                }
+            }
+            data.splice(id, 0, data.splice(0, 1)[0]);
+            player.loadTrack(id, data);
+        }
+    });   
+}
+
 player.loadTrack(0);
 
 
@@ -166,6 +226,7 @@ player.buttonMute.addEventListener('click',player.muteUnmute,false);
 player.audio.addEventListener('timeupdate',player.updateProgress,false);
 player.buttonNext.addEventListener('click',player.nextTrack,false);
 player.buttonPrev.addEventListener('click',player.prevTrack,false);
+player.buttonShuffle.addEventListener('click',player.shuffle,false);
 player.pB.on('click',player.setTime);
  
 });
