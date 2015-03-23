69af586337c838d3e880df93929e7ea9a04f73b5
helenep <helene.pruvot1@gmail.com>
2015-03-23 10:05:56 +0100

    remove comments

--- app/assets/js/player.js
+++ app/assets/js/player.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 var index = 0,
-    playing = false;
     mediaPath = 'http://localhost:3000/assets/media/',
     coverPath = 'http://localhost:3000/assets/images/Covers/',
     extension = '.mp3',
@@ -144,13 +143,6 @@ player.loadTrack = function(id) {
         }
     }
 
-    // player.toFollow() = function(data) {
-    //     $("#plUL");
-    // }
-
-    // $('.plSel').removeClass('plSel');
-    // $('#plUL li:eq(' + id + ')').addClass('plSel');
-
 }
 
 
@@ -168,9 +160,6 @@ player.muteUnmute = function() {
 
 player.loadTrack(0);
 
-// $("#audio-next").on("click",function(){
-//     console.log(tracks[].track);
-// })
 
 player.button.addEventListener('click',player.playPause,false);
 player.buttonMute.addEventListener('click',player.muteUnmute,false);
@@ -179,88 +168,4 @@ player.buttonNext.addEventListener('click',player.nextTrack,false);
 player.buttonPrev.addEventListener('click',player.prevTrack,false);
 player.pB.on('click',player.setTime);
  
-
-// jQuery(function($) {
-// var supportsAudio = !!document.createElement('audio').canPlayType;
-
-// if(supportsAudio) {
-//         var index = 0,
-//         playing = false;
-//         mediaPath = 'assets/media/',
-//         extension = '',
-//         tracks = [
-//             {"track":1,"name":"L'apologie","length":"00:55","file":"Céline Dion - I_m Alive"},
-//             {"track":2,"name":"Les Moutons","length":"00:37","file":"Fever Ray _Keep The Streets Empty For Me_"},
-//             {"track":3,"name":"La ville de La Rochelle","length":"01:05","file":"Sexual Healing (Remix)"},
-//             {"track":4,"name":"Les Echevins de Nantes","length":"00:40","file":"04"}
-//         ],
-//         trackCount = tracks.length,
-//         npTitle = $('#asaTitle'),
-//         audio = $('#audio_player').bind('play', function() {
-//             playing = true;
-//         }).bind('pause', function() {
-//             playing = false;
-//         }).bind('ended', function() {
-//             if((index + 1) < trackCount) {
-//                 index++;
-//                 loadTrack(index);
-//                 audio.play();
-//             } else {
-//                 audio.pause();
-//                 index = 0;
-//                 loadTrack(index);
-//             }
-//         }).get(0),
-//         btnPrev = $('.audio-prev').click(function() {
-//             if((index - 1) > -1) {
-//                 index--;
-//                 loadTrack(index);
-//                 if(playing) {
-//                     audio.play();
-//                 }
-//             } else {
-//                 audio.pause();
-//                 index = 0;
-//                 loadTrack(index);
-//             }
-//         }),
-//         btnNext = $('.audio-next').click(function() {
-//             if((index + 1) < trackCount) {
-//                 index++;
-//                 loadTrack(index);
-//                 if(playing) {
-//                     audio.play();
-//                 }
-//             } else {
-//                 audio.pause();
-//                 index = 0;
-//                 loadTrack(index);
-//             }
-//         }),
-//         li = $('#plUL li').click(function() {
-//             var id = parseInt($(this).index());
-//             if(id !== index) {
-//                 playTrack(id);
-//             }
-//         }),
-//         loadTrack = function(id) {
-//             $('.plSel').removeClass('plSel');
-//             $('#plUL li:eq(' + id + ')').addClass('plSel');
-//             npTitle.text(tracks[id].name);
-//             index = id;
-//             audio.src = mediaPath + tracks[id].file + extension;
-//         },
-//         playTrack = function(id) {
-//             loadTrack(id);
-//             audio.play();
-//         };
-//         if(audio.canPlayType('audio/ogg')) {
-//             extension = '.ogg';
-//         }
-//         if(audio.canPlayType('audio/mpeg')) {
-//             extension = '.mp3';
-//         }
-//         loadTrack(index);
-//     }
-// });
 });
